30f4f5aea63bf15a06d594cda8c26f3cb1e000b3
LiMingCong <2058615288@qq.com>
2025-09-15 10:20:38 +0800

    初始化项目

--- src/settings.js
+++ src/settings.js
@@ -1,16 +1,18 @@
+// 负责导出网站基础配置。包括：网站标题、固定header、显示logo
+
 module.exports = {
 
-  title: 'Vue Admin Template',
+  title: 'LMC人力资源后台管理系统', // 标题 网站的标题
 
   /**
    * @type {boolean} true | false
    * @description Whether fix the header
    */
-  fixedHeader: false,
+  fixedHeader: true, // 是否固定头部
 
   /**
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
-  sidebarLogo: false
+  sidebarLogo: true // 是否显示侧边栏的logo
 }
