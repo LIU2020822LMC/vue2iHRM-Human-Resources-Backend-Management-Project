ddc0186faff7d5e6a0049b298182602f3ed29f7c
LiMingCong <2058615288@qq.com>
2025-09-19 18:27:34 +0800

    实现登录功能联调

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,10 @@
-import request from '@/utils/request'
+import request from '@/utils/request.js'
 
-export function login(data) {
+export const login = (data) => {
   return request({
-    url: '/vue-admin-template/user/login',
-    method: 'post',
-    data
+    url: 'sys/login',
+    method: 'POST',
+    data: data
   })
 }
 
-export function getInfo(token) {
-  return request({
-    url: '/vue-admin-template/user/info',
-    method: 'get',
-    params: { token }
-  })
-}
-
-export function logout() {
-  return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
-  })
-}
