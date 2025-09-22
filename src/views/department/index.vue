dfc0047c60d4a694ca0e3d4c654143a77dd7ce06
LiMingCong <2058615288@qq.com>
2025-09-22 20:21:48 +0800

    组织架构-树形自定义结构

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -2,7 +2,25 @@
   <div class="container">
     <div class="app-container">
       <!-- 展示树形结构 -->
-      <el-tree :data="depts" :props="defaultProps" default-expand-all />
+      <el-tree :data="depts" :props="defaultProps" default-expand-all>
+        <!-- 节点结构 -->
+        <el-row style="width: 100%;height: 40px;" type="flex" justify="space-between" align="middle">
+          <el-col>传智教育</el-col>
+          <el-col :span="4">
+            <span class="tree-manager">管理员</span>
+            <el-dropdown>
+              <span class="el-dropdown-link">
+                操作<i class="el-icon-arrow-down el-icon--right" />
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>添加子部门</el-dropdown-item>
+                <el-dropdown-item>编辑部门</el-dropdown-item>
+                <el-dropdown-item>删除</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </el-col>
+        </el-row>
+      </el-tree>
     </div>
   </div>
 </template>
@@ -43,5 +61,12 @@ export default {
   .app-container{
     padding: 30px 140px;
     font-size: 14px;
+  }
+  .tree-manager{
+    width: 50px;
+    /*让元素既保持 ** inline 元素的同行特性 **（不独占一行，可与其他元素并排），又具备 ** block 元素的尺寸可控特性 **（可设置宽高、垂直方向的 margin/padding 等）。 */
+    /* inline-block 解决了 "既想让元素并排显示，又想精确控制其盒模型" 的需求。 */
+    display: inline-block;
+    margin: 10px;
   }
 </style>
