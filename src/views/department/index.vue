6ebdcf9d26fff5c710a1341b1575cf470d4f006f
LiMingCong <2058615288@qq.com>
2025-09-22 21:13:55 +0800

    获取组织架构数据

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -8,7 +8,7 @@
           <el-row style="width: 100%;height: 40px;" type="flex" justify="space-between" align="middle">
             <el-col>{{ data.name }}</el-col>
             <el-col :span="4">
-              <span class="tree-manager">{{ data.ManagerName }}</span>
+              <span class="tree-manager">{{ data.managerName }}</span>
               <el-dropdown>
                 <!-- 显示区域内容 -->
                 <span class="el-dropdown-link">
@@ -29,37 +29,30 @@
   </div>
 </template>
 <script>
+import { getDepartment } from '@/api/department'
+
 export default {
   name: 'Department',
   data() {
     return {
-      depts: [
-        {
-          name: '传智教育',
-          ManagerName: '张三',
-          children: [
-            { name: '总裁办', ManagerName: '李四' },
-            { name: '行政部', ManagerName: '王五' },
-            {
-              name: '人事部',
-              ManagerName: '小梅',
-              children: [{
-                name: '财务核算部'
-              },
-              {
-                name: '税务管理部'
-              },
-              {
-                name: '薪资管理部'
-              }]
-            }]
-        }],
+      depts: [],
       defaultProps: {
         children: 'children',
         label: 'name'
       }
     }
+  },
+  created() {
+    this.getDepartment()
+  },
+  methods: {
+    async getDepartment() {
+      const res = await getDepartment()
+      this.depts = res
+      console.log(res)
+    }
   }
+
 }
 </script>
 
@@ -73,6 +66,6 @@ export default {
     /*让元素既保持 ** inline 元素的同行特性 **（不独占一行，可与其他元素并排），又具备 ** block 元素的尺寸可控特性 **（可设置宽高、垂直方向的 margin/padding 等）。 */
     /* inline-block 解决了 "既想让元素并排显示，又想精确控制其盒模型" 的需求。 */
     display: inline-block;
-    margin: 10px;
+    margin-right: 40px;
   }
 </style>
