f7b9535fb718c94422490f3858c86dd8471dd736
LiMingCong <2058615288@qq.com>
2025-09-23 14:09:28 +0800

    添加子部门-表单结构

--- src/views/department/components/add-dept.vue
+++ src/views/department/components/add-dept.vue
@@ -1,7 +1,29 @@
 <template>
   <el-dialog title="新增部门" :visible="showDialogVisible" @close="close">
     <!-- 放置表单 -->
-    123
+    <el-form label-width="120px">
+      <el-form-item label="部门名称">
+        <el-input style="width: 80%;" />
+      </el-form-item>
+      <el-form-item label="部门编码">
+        <el-input style="width: 80%;" />
+      </el-form-item>
+      <el-form-item label="部门负责人">
+        <el-select style="width: 80%;" />
+      </el-form-item>
+      <el-form-item label="部门介绍">
+        <el-input type="textarea" :rows="5" style="width: 80%;" />
+      </el-form-item>
+      <el-form-item>
+        <el-row type="flex" justify="center">
+          <el-col :span="12">
+            <el-button type="primary" @click="onSubmit">确定</el-button>
+            <el-button>取消</el-button>
+          </el-col>
+        </el-row>
+
+      </el-form-item>
+    </el-form>
   </el-dialog>
 </template>
 
