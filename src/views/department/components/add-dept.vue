4c90155d4019f371dea7efdca915895305be981e
LiMingCong <2058615288@qq.com>
2025-09-24 01:34:23 +0800

    组织架构-编辑部门-确认取消

--- src/views/department/components/add-dept.vue
+++ src/views/department/components/add-dept.vue
@@ -1,21 +1,21 @@
 <template>
-  <el-dialog title="新增部门" :visible="showDialogVisible" @close="close">
+  <el-dialog :title="showTitle" :visible="showDialogVisible" @close="close">
     <!-- 放置表单 -->
-    <el-form ref="addDept" :model="addForm" :rules="rules" label-width="120px">
+    <el-form ref="addDept" :model="departmentForm" :rules="rules" label-width="120px">
       <el-form-item label="部门名称" prop="name">
-        <el-input v-model="addForm.name" placeholder="2-10个字符" style="width: 80%;" />
+        <el-input v-model="departmentForm.name" placeholder="2-10个字符" style="width: 80%;" />
       </el-form-item>
       <el-form-item label="部门编码" prop="code">
-        <el-input v-model="addForm.code" placeholder="2-10个字符" style="width: 80%;" />
+        <el-input v-model="departmentForm.code" placeholder="2-10个字符" style="width: 80%;" />
       </el-form-item>
       <el-form-item label="部门负责人" prop="managerId">
         <!-- 选择器双向绑定的是el-option中的value，即:value="item.id" -->
-        <el-select v-model="addForm.managerId" placeholder="请选择负责人" style="width: 80%;">
+        <el-select v-model="departmentForm.managerId" placeholder="请选择负责人" style="width: 80%;">
           <el-option v-for="item in ManagerList" :key="item.id" :label="item.username" :value="item.id" />
         </el-select>
       </el-form-item>
       <el-form-item label="部门介绍" prop="introduce">
-        <el-input v-model="addForm.introduce" placeholder="1-100个字符" type="textarea" :rows="5" style="width: 80%;" />
+        <el-input v-model="departmentForm.introduce" placeholder="1-100个字符" type="textarea" :rows="5" style="width: 80%;" />
       </el-form-item>
       <el-form-item>
         <el-row type="flex" justify="center">
@@ -31,7 +31,7 @@
 </template>
 
 <script>
-import { getDepartment, getManagerList, addDepartment, getDepartmentDetail } from '@/api/department.js'
+import { getDepartment, getManagerList, addDepartment, getDepartmentDetail, updateDepartment } from '@/api/department.js'
 
 export default {
   name: 'AddDept',
@@ -48,7 +48,7 @@ export default {
   data() {
     return {
       ManagerList: [],
-      addForm: {
+      departmentForm: {
         code: '', // 部门编码
         introduce: '', // 部门介绍
         managerId: '', // 部门负责人名字
@@ -62,9 +62,9 @@ export default {
             trigger: 'blur',
             validator: async(rule, value, callback) => {
               let res = await getDepartment()
-              // 用addForm是否拥有id来判断是编辑模式还是新增模式，有的话就是编辑模式
-              if (this.addForm.id) {
-                res = res.filter(item => item.id !== this.addForm.id)
+              // 用departmentForm是否拥有id来判断是编辑模式还是新增模式，有的话就是编辑模式
+              if (this.departmentForm.id) {
+                res = res.filter(item => item.id !== this.departmentForm.id)
               }
               if (res.some(item => item.code === value)) {
                 callback(new Error('部门编码已存在'))
@@ -84,9 +84,9 @@ export default {
             trigger: 'blur',
             validator: async(rule, value, callback) => {
               let res = await getDepartment()
-              // 用addForm是否拥有id来判断是编辑模式还是新增模式，有的话就是编辑模式
-              if (this.addForm.id) {
-                res = res.filter(item => item.id !== this.addForm.id)
+              // 用departmentForm是否拥有id来判断是编辑模式还是新增模式，有的话就是编辑模式
+              if (this.departmentForm.id) {
+                res = res.filter(item => item.id !== this.departmentForm.id)
               }
               if (res.some(item => item.name === value)) {
                 callback(new Error('部门名称已存在'))
@@ -99,6 +99,11 @@ export default {
       }
     }
   },
+  computed: {
+    showTitle() {
+      return this.departmentForm.id ? '编辑部门' : '新增部门'
+    }
+  },
   created() {
     this.GetManagerList()
   },
@@ -106,7 +111,18 @@ export default {
 
     // 新增部门叉号关闭传递父组件数据事件
     close() {
-      this.$refs.addDept.resetFields() // 重置表单
+      // 手动清除表单的数据，即重新置空
+      this.departmentForm = {
+        code: '', // 部门编码
+        introduce: '', // 部门介绍
+        managerId: '', // 部门负责人名字
+        name: '', // 部门名称
+        pid: '' // 部门父级部门id
+      }
+
+      // 重置表单，只能重置绑定的表单数据，即this.departmentForm
+      // resetFields()作用之一：清除表单的验证状态（比如清除校验错误提示、重置字段的 touched 状态等）
+      this.$refs.addDept.resetFields()
       this.$emit('close', false)
     },
 
@@ -121,8 +137,15 @@ export default {
       // 判断全部校验是否通过
       this.$refs.addDept.validate(async(isOK) => {
         if (isOK) {
-          await addDepartment({ ...this.addForm, pid: this.currentNodeId })
-          this.$message.success('新增部门成功')
+          let msg = '新增'
+          // 通过this.departmentForm是否有id来判断是编辑还是新增
+          if (this.departmentForm.id) {
+            await updateDepartment(this.departmentForm)
+            msg = '修改'
+          } else {
+            await addDepartment({ ...this.departmentForm, pid: this.currentNodeId })
+          }
+          this.$message.success(`${msg}部门成功`)
           this.$emit('updateDepartment')
           this.close()
         }
@@ -132,7 +155,7 @@ export default {
     // 获取部门详情
     async GetDepartmentDetail() {
       const res = await getDepartmentDetail(this.currentNodeId)
-      this.addForm = res
+      this.departmentForm = res
     }
   }
 }
