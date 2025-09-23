42ca0ff82116f424b0bfa5ca4b31644a88f37465
LiMingCong <2058615288@qq.com>
2025-09-23 15:11:37 +0800

    组织架构-添加子部门-负责人数据

--- src/api/department.js
+++ src/api/department.js
@@ -1,7 +1,20 @@
 import request from '@/utils/request'
 
+/**
+ *
+ * 获取组织架构数据
+ */
 export const getDepartment = () => {
   return request({
     url: 'company/department'
+  })
+}
+
+/**
+ * 获取部门负责人列表数据
+ */
+export const getManagerList = () => {
+  return request({
+    url: '/sys/user/simple'
   })
 }
