509ab56831205c1ed636cfb8101c3c0c54bb2e55
LiMingCong <2058615288@qq.com>
2025-09-24 18:03:23 +0800

    角色管理-添加角色弹层

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -3,7 +3,7 @@
     <div class="app-container">
       <!-- 放置添加角色按钮 -->
       <div class="role-operate">
-        <el-button type="primary">添加角色</el-button>
+        <el-button type="primary" @click="dialogVisible = true">添加角色</el-button>
       </div>
 
       <!-- 放置表格 -->
@@ -39,6 +39,25 @@
         </el-column>
       </el-row>
 
+      <!-- 放置弹窗 -->
+      <!-- 通过 .sync 修饰符，当子组件内部改变 visible 状态时（比如点击关闭按钮），会自动同步更新父组件的 dialogVisible 变量 -->
+      <el-dialog :visible.sync="dialogVisible" title="新增角色" width="500px">
+        <el-form  label-width="120px">
+          <el-form-item label="角色名称">
+            <el-input style="width: 300px;" />
+          </el-form-item>
+          <el-form-item label="启用">
+            <el-switch />
+          </el-form-item>
+          <el-form-item label="角色描述">
+            <el-input type="textarea" :row="5" style="width: 300px;" />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">立即创建</el-button>
+            <el-button>取消</el-button>
+          </el-form-item>
+        </el-form>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -51,21 +70,24 @@ export default {
     return {
       roleList: [],
       pageParams: {
-        page: 1,
-        pagesize: 5,
-        total: 0
-      }
+        page: 1, // 当前页
+        pagesize: 5, // 每页显示的数据数量
+        total: 0 // 总数据量
+      },
+      dialogVisible: false
     }
   },
   created() {
     this.GetRoleList()
   },
   methods: {
+    // 获取角色列表函数
     async GetRoleList() {
       const { rows, total } = await getRoleList(this.pageParams)
       this.roleList = rows
       this.pageParams.total = total
     },
+    // 当前页的请求函数
     CurrentPageChange(currentPage) {
       this.pageParams.page = currentPage
       this.GetRoleList()
