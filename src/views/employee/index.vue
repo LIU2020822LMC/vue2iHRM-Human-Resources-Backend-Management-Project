ae11e4c8e02becea29bf6edd685272d46c7b4039
LiMingCong <2058615288@qq.com>
2025-09-26 11:44:48 +0800

    员工管理-记录树节点

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -11,11 +11,14 @@
         />
         <!-- 树形组件 -->
         <el-tree
+          ref="deptTree"
+          node-key="id"
           :data="departmentList"
           :props="defaultProps"
           :highlight-current="true"
           default-expand-all
           :expand-on-click-node="false"
+          @current-change="SelectNode"
         />
       </div>
       <div class="right">
@@ -42,6 +45,11 @@ export default {
       defaultProps: {
         children: 'children',
         label: 'name'
+      },
+
+      // 存储查询参数
+      queryParams: {
+        departmentId: null
       }
     }
   },
@@ -54,6 +62,19 @@ export default {
       const res = await getDepartment()
       // 将部门列表数据进行排列
       this.departmentList = transListToTreeData(res, 0)
+      this.queryParams.departmentId = this.departmentList[0].id
+
+      // 等前面的执行完了才执行下面的回调函数
+      this.$nextTick(() => {
+        // 设置当前node-key的指定字段的值以及加载完成后选中第一个节点（因为此时的this.queryParams.departmentId是this.departmentList[0].id）
+        this.$refs.deptTree.setCurrentKey(this.queryParams.departmentId)
+      })
+      // console.log(this.queryParams.departmentId)
+    },
+    SelectNode(node) {
+      // node参数是树形控件里面的每一个对象数据
+      this.queryParams.departmentId = node.id
+      // console.log(this.queryParams.departmentId)
     }
   }
 
