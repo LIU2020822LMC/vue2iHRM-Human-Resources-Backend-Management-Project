9d531814fcdcc466754858722f5f7297f3500737
LiMingCong <2058615288@qq.com>
2025-09-29 15:56:27 +0800

    权限管理-搭建页面结构

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -2,6 +2,19 @@
   <div class="container">
     <div class="app-container">
       <el-button type="primary" size="mini " class="btn-add">添加权限</el-button>
+
+      <!-- 放置表格 -->
+      <el-table>
+        <el-table-column label="名称" align="center" />
+        <el-table-column label="标识" align="center" />
+        <el-table-column label="描述" align="center" />
+        <el-table-column label="操作" align="center" />
+        <el-table-column>
+          <el-button type="text">添加</el-button>
+          <el-button type="text">编辑</el-button>
+          <el-button type="text">清除</el-button>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
