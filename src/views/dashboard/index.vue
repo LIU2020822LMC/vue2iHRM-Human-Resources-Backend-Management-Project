f83e4a638b7b1878c6fc85d8f9ecc71459c9aa9d
LiMingCong <2058615288@qq.com>
2025-10-10 14:35:34 +0800

    首页-图表的按需导入

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -194,7 +194,20 @@
 import CountTo from 'vue-count-to'
 import { mapGetters } from 'vuex'
 import { getHomeData, getHomeNotic } from '@/api/home'
-import * as echarts from 'echarts' // 引入所有的echarts
+// import * as echarts from 'echarts' // 引入所有的echarts
+// 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
+import * as echarts from 'echarts/core'
+// 引入折线图图表，图表后缀都为 Chart
+import { LineChart } from 'echarts/charts'
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+import { CanvasRenderer } from 'echarts/renderers'
+import { GridComponent } from 'echarts/components'
+// 注册必须的组件
+echarts.use([
+  GridComponent,
+  LineChart,
+  CanvasRenderer
+])
 
 export default {
   components: {
